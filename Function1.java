ee34b5ee6cce1fa28e17f8ced63e1753ae2f80ab
Mohit <mrai645@gmail.com>
2023-07-12 21:52:43 +0530

    even odd

--- Function1.java
+++ Function1.java
@@ -1,27 +1,57 @@
 import java.util.Scanner;
 
 public class Function1 {
-    public static boolean isPrime(int n){
-        if(n<=1)
-        return false;
 
-        for(int i=2;i<n;i++)
-        if(n%i==0)
-        return false;
+// @Wap for number is odd or even
 
-        return true;
+    public static boolean isEven(int n){
+        // for(int i=0;i<=n;i++)
+            if(n%2==0){
+                return true;
+            }
+           
+        
+         return false;
     }
 
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
+        Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
 
-        if(isPrime(n)){
-            System.out.println("Prime");
+        if(isEven(n)){
+            System.out.println("even");
+
+        }else{
+            System.out.println("odd");
         }
-        else{
-            System.out.println("not prime");
-        }
-        isPrime(n);
+        isEven(n);
     }
+
+
+
+
+    // public static boolean isPrime(int n){
+    //     if(n<=1)
+    //     return false;
+
+    //     for(int i=2;i<n;i++)
+    //     if(n%i==0)
+    //     return false;
+
+    //     return true;
+    // }
+
+    // public static void main(String args[]){
+    //     Scanner sc = new Scanner(System.in);
+    //     int n=sc.nextInt();
+
+    //     if(isPrime(n)){
+    //         System.out.println("Prime");
+    //     }
+    //     else{
+    //         System.out.println("not prime");
+    //     }
+    //     isPrime(n);
+    // }
 }
+
