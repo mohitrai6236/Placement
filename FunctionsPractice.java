01e379469869d121cacf826e55120161d1880792
Mohit <mrai645@gmail.com>
2023-07-13 22:49:50 +0530

    odd sum

--- FunctionsPractice.java
+++ FunctionsPractice.java
@@ -3,19 +3,44 @@ import java.util.*;
 
 
 public class FunctionsPractice {
-         public static int average(int a,int b,int c){
-          return (a+b+c)/3;
-         }    
 
-         public static void main(String args[]){
+// @2 Wap to print sum odd numbers
 
-            Scanner sc=new Scanner(System.in);
+    public static void oddNumbers(int n){
+
+       int sum=0;
+       for(int i=1;i<=n;i++){
+        if(i%2!=0){
+            sum=sum+i;
+        }
+       }
+       System.out.println(sum);
+    }
+
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+
+
+        oddNumbers(n);
+    }
+
+    //@1 Wap to print average of three numbers using functions
+
+        //  public static int average(int a,int b,int c){
+        //   return (a+b+c)/3;
+        //  }    
+
+        //  public static void main(String args[]){
+
+        //     Scanner sc=new Scanner(System.in);
             
-            int a=sc.nextInt();
-            int b=sc.nextInt();
-            int c=sc.nextInt();
+        //     int a=sc.nextInt();
+        //     int b=sc.nextInt();
+        //     int c=sc.nextInt();
 
-            System.out.println(average(a,b,c)); // When their is only one input then we use function calling from main
-            //but when their is multiple input than we use syso in main function
-         }
+        //     System.out.println(average(a,b,c)); // When their is only one input then we use function calling from main
+        //     //but when their is multiple input than we use syso in main function
+        //  }
 }
