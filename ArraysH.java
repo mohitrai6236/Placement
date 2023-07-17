3219039682c1ab573e1cfa93bcc65dac117ea8fd
Mohit <mrai645@gmail.com>
2023-07-17 21:31:52 +0530

    maximum minimum

--- ArraysH.java
+++ ArraysH.java
@@ -1,18 +1,51 @@
 import java.util.*;
 public class ArraysH {
-    public static void main(String args[]){
-        Scanner sc =new Scanner(System.in);
-        int size = sc.nextInt();
-        String names[]= new String[size];
+   
+//    ______________________________________________________________________________________
+//    @2 Wap to Find the maximum & minimum number in an array of integers.
+    
+//    public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         int size = sc.nextInt();
+//         int numbers[] = new int[size];
 
-        //Input
-        for(int i=0;i<size;i++){
-            names[i]=sc.next();
-        }
+//         //input
 
-        //output
-        for(int i=0;i<names.length;i++){
-            System.out.println("name is : " +names[i]);
-        }
-    }
+//         for(int i=0;i<size;i++){
+//             numbers[i] = sc.nextInt();
+//         }
+
+//         int max = Integer.MIN_VALUE;
+//         int min=Integer.MAX_VALUE;
+
+//         for(int i=0;i<numbers.length;i++){
+//             if(numbers[i]>max){
+//                 max=numbers[i];
+//             }
+//             if(numbers[i]<min){
+//                 min=numbers[i];
+//             }
+
+//         }
+//         System.out.println("largest number is : " + max);
+//         System.out.println("Smallest number is : " + min);
+//     }
+  
+//     _______________________________________________________________________________________
+//    @1 Wap to print names
+    // public static void main(String args[]){
+    //     Scanner sc =new Scanner(System.in);
+    //     int size = sc.nextInt();
+    //     String names[]= new String[size];
+
+    //     //Input
+    //     for(int i=0;i<size;i++){
+    //         names[i]=sc.next();
+    //     }
+
+    //     //output
+    //     for(int i=0;i<names.length;i++){
+    //         System.out.println("name is : " +names[i]);
+    //     }
+    // }
 }
