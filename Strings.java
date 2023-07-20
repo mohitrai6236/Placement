8b045ccb69be7ce5166f0080f71563d56c47b0e7
Mohit <mrai645@gmail.com>
2023-07-20 21:23:03 +0530

    String

--- Strings.java
+++ Strings.java
@@ -1,20 +1,57 @@
 import java.util.*;
 public class Strings {
     public static void main(String args[]){
+________________________________________________________________________________________________________________________________________
 
-        // @3 Concatenation
+        // @6 Wap to print the number from where we want to print using the numbers
 
-        String firstName = "Tony";
-        String lastName = "Stark";
-        String fullName = firstName + " " + lastName;
-        System.out.println(fullName.length()); // It use to print that how many character are their in a sentence
-        // System.out.println(fullName); // It is use to print the name
+        // String sentence ="TonyStark";
+        // String name = sentence.substring(4);
+        // System.out.println(name);
+
+
+
+
+
+
+    //@5    //compare
+        String name1 = "Tony";
+        String name2 = "Tony2";
+
+        //1 s1 > s2 : +ve value
+        //2 s1 == s2 : 0;
+        //3 s1 < s2 : -ve value
+
+        // if(name1.compareTo(name2) == 0){     //This is the correct way to compare in java
+        //     System.out.println("String are equal");
+        // }else {
+        //     System.out.println("String are not equal");
+        // }
+
+//_______________________________________________________________________________________________________________
+        // This is not a correct way to compare in java but it will run sometime only
+
+        // if(name1 == name2){
+        //     System.out.println("String are equal");
+        //  }else {
+        //      System.out.println("String are not equal");
         
-        // @4 charAt
+        // }
 
-        for(int i=0;i<fullName.length();i++){
-            System.out.println(fullName.charAt(i));// this is used to find out what character will come at what place
-        }
+    // _______________________________________________________________________________________________________________________________  
+    //     // @3 Concatenation
+
+    //     String firstName = "Tony";
+    //     String lastName = "Stark";
+    //     String fullName = firstName + " " + lastName;
+    //     System.out.println(fullName.length()); // It use to print that how many character are their in a sentence
+    //     // System.out.println(fullName); // It is use to print the name
+    // __________________________________________________________________________________________________________________________________________    
+    //     // @4 charAt
+
+    //     for(int i=0;i<fullName.length();i++){
+    //         System.out.println(fullName.charAt(i));// this is used to find out what character will come at what place
+    //     }
        
 
 
@@ -24,7 +61,7 @@ public class Strings {
 
 
 // ________________________________________________________________________________________________________________________________________
-// @1 Wap on how to write a full sentence using String in java
+// @2 Wap on how to write a full sentence using String in java
 //          Scanner sc = new Scanner(System.in);
 //         String name = sc.nextLine(); // nextLine is used to print the whole line but for one word we use only next
 //         System.out.println("Tour name is : "+ name);
