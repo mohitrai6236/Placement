45db1cd9926540bc597cbfd5574db936bea75931
Mohit <mrai645@gmail.com>
2023-07-24 21:38:53 +0530

    string builder position finder

--- StringsB.java
+++ StringsB.java
@@ -2,5 +2,8 @@ public class StringsB {
     public static void main(String args[]){
         StringBuilder sb = new StringBuilder("Tony");
         System.out.println(sb);
+
+        //find the charater at index 0
+        System.out.println(sb.charAt(0));// 0 define what letter is that position
     }
 }
