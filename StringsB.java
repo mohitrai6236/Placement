7303fd51f73c894a32f3eb310fabbcc320f086ee
Mohit <mrai645@gmail.com>
2023-07-24 22:14:25 +0530

    insert string

--- StringsB.java
+++ StringsB.java
@@ -2,13 +2,19 @@ public class StringsB {
     public static void main(String args[]){
         StringBuilder sb = new StringBuilder("Tony");
         System.out.println(sb);
+// _______________________________________________________________________________________________________________
+//        @1 //find the charater at index 0
+//         System.out.println(sb.charAt(0));// 0 define what letter is that position
+// ________________________________________________________________________________________________________________
+//        @2 //set char at index 0
 
-        //find the charater at index 0
-        System.out.println(sb.charAt(0));// 0 define what letter is that position
-
-        //set char at index 0
-
-        sb.setCharAt(0, 'P');
-        System.out.println(sb);
+//         sb.setCharAt(0, 'P');// it will replace the letters
+//         System.out.println(sb);
+      
+            
+           sb.insert(2,'n');
+            
+            //sb.insert(0, 'S');
+            System.out.println(sb);// it is used to insert the char
     }
 }
