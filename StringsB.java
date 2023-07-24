56f6bd35f2643e75375b196db84bf67801892362
Mohit <mrai645@gmail.com>
2023-07-24 22:26:26 +0530

    append string

--- StringsB.java
+++ StringsB.java
@@ -1,7 +1,15 @@
 public class StringsB {
     public static void main(String args[]){
         StringBuilder sb = new StringBuilder("Tony");
-        System.out.println(sb);
+        // for 1,2,3,4 only System.out.println(sb);
+sb.append("e");
+sb.append("l");
+sb.append("l");
+sb.append("0");
+System.out.println(sb);
+        // @5 append the char means add char at the end of the word
+
+
 // _______________________________________________________________________________________________________________
 //        @1 //find the charater at index 0
 //         System.out.println(sb.charAt(0));// 0 define what letter is that position
@@ -12,16 +20,16 @@ public class StringsB {
 //         System.out.println(sb);
    //____________________________________________________________________________________________________________________________
 
-   // @3 insert at the given place         
-           sb.insert(2,'n');
+//    // @3 insert at the given place         
+//            sb.insert(2,'n');
             
-            //sb.insert(0, 'S');
-            System.out.println(sb);// it is used to insert the char
-// _______________________________________________________________________________________________________________
-            // @4 delete the extrea n
+//             //sb.insert(0, 'S');
+//             System.out.println(sb);// it is used to insert the char
+// // _______________________________________________________________________________________________________________
+//             // @4 delete the extrea n
 
-            sb.delete(2, 3);
-            System.out.println(sb);
+//             sb.delete(2, 3);
+//             System.out.println(sb);
 
     }
 }
