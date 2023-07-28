b262e3795b5e7f1f0dc9fc8ce063607c47fba1f0
Mohit <mrai645@gmail.com>
2023-07-28 20:27:56 +0530

    Create bits1.java

---
+++ lecture14/bits1.java
@@ -0,0 +1,11 @@
+package lecture14;
+
+public class bits1 {
+    public static void main(String args[]){
+        int n=5;
+        int pos=1;
+        int bitMask=1<<pos;
+        int newNumber=bitMask | n;
+        System.out.println(newNumber);
+    }
+}
