add66543824cbebf216bf53d013de5d9fa895a79
Mohit <mrai645@gmail.com>
2023-08-15 22:30:12 +0530

    Create recursion2.java

---
+++ Recursion17/recursion2.java
@@ -0,0 +1,15 @@
+package Recursion17;
+
+public class recursion2 {
+    public static void printSum(int n, int sum) {
+        if(n == 0) {
+        System.out.println(sum);
+        return;
+        }
+        sum += n;
+        printSum(n-1, sum);
+        }
+        public static void main(String args[]) {
+        printSum(5, 0);
+        }
+}
