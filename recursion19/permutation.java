d67ea521cdf84f4a594a87c078399ecb7d827849
Mohit <mrai645@gmail.com>
2023-08-31 23:03:10 +0530

    calculate permutation

---
+++ recursion19/permutation.java
@@ -0,0 +1,32 @@
+package recursion19;
+
+public class permutation {
+    public static void printPerm(String str, String permutation) {
+        if(str.length() == 0) {
+            System.out.println(permutation);
+            return;
+
+        }
+
+        for(int i=0; i<str.length(); i++) {
+            char currChar = str.charAt(i);
+            //"abc" = "ab"
+            String newStr = str.substring(0, i) + str.substring(i+1);
+            printPerm(newStr, permutation+currChar);
+
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = "abc";
+        printPerm(str, "");
+    }
+}
+
+// output
+// abc
+// acb
+// bac
+// bca
+// cab
+// cba
