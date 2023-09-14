30fa4a29de3e896721b31f27e791dd0c336fd883
Mohit <mrai645@gmail.com>
2023-09-14 20:56:21 +0530

    Update tempCodeRunnerFile.java

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,5 +1,27 @@
- int a=10;
-        int b=0;
-        b=a++;
-        System.out.println(a);
-        System.out.println(b);
+public static void main(String args[]){
+        Scanner sc =  new Scanner(System.in);
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+
+        int [][] numbers =new int[rows][cols];
+
+        //input
+        ///rows
+
+        for(int i=0;i<rows;i++){
+            //columns
+            for(int j=0;j<cols;j++){
+                numbers[i][j] = sc.nextInt();
+            }
+        }
+        int x = sc.nextInt();
+        
+        //output
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(numbers[i][j] == x){
+                    System.out.println("x found at location("+i+","+j+")");
+                }
+            }
+        }
+    }
