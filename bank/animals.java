4ca33fc7b01493457dbfc333db19e75fb934c027
Mohit <mrai645@gmail.com>
2023-09-22 20:55:53 +0530

    Create animals.java

---
+++ bank/animals.java
@@ -0,0 +1,22 @@
+package bank;
+abstract class Animal {
+    abstract void walk();
+}
+
+class Horse extends Animal {
+    public void walk() {
+        System.out.println("walks on 4 legs");
+    }
+}
+
+class Chicken extends Animal {
+    public void walk() {
+        System.out.println("walks on two legs");
+    }
+}
+public class animals {
+    public static void main(String[] args) {
+        Horse horse = new Horse();
+        horse.walk();
+    }
+}
