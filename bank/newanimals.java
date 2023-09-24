e0f802b6d55b62bfd04694ef8e43347f406f3208
Mohit <mrai645@gmail.com>
2023-09-24 20:18:23 +0530

    Create newanimals.java

---
+++ bank/newanimals.java
@@ -0,0 +1,32 @@
+package bank;
+abstract class Animal {
+    abstract void walk();
+    Animal() {
+        System.out.println("You are creating a new animal");
+    }
+    public void eat() {
+        System.out.println("Animal eats");
+    }
+}
+
+class Horse extends Animal {
+    Horse() {
+        System.out.println("Created a horse");
+    }
+    public void walk() {
+        System.out.println("walks on 4 legs");
+    }
+}
+
+class Chicken extends Animal {
+    
+    public void walk() {
+        System.out.println("Walks on 2 legs");
+    }
+}
+
+public class newanimals {
+    public static void main(String[] args) {
+        Horse horse = new Horse();
+    }
+}
