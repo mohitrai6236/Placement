3e581b39a4a7b5b66d4fa8e39550bf5ee6d59c4d
Mohit <mrai645@gmail.com>
2023-09-30 22:19:03 +0530

    Create oops2.java

---
+++ lecture24/oops2.java
@@ -0,0 +1,42 @@
+package lecture24;
+class Pen{
+        String color;
+        String type;
+    
+        public void write() {
+            System.out.println("writing something");
+    
+        }
+        public void printColor() {
+            System.out.println(this.color); // this is used for which function has called it
+        }
+    }
+    
+    class Student {
+        String name;
+        int age;
+    
+        public void printInfo() {
+            System.out.println(this.name);
+            System.out.println(this.age);
+        }
+
+        Student(Student s2) {
+            this.name=s2.name;
+            this.age=s2.age;
+        }
+
+        Student() {
+            
+        }
+    }
+public class oops2 {
+       public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "aman";
+        s1.age = 24;
+
+        Student s2 = new Student(s1);
+        s2.printInfo();
+       }
+}
