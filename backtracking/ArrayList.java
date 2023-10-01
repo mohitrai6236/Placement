e04c3ce52fb25c1174342cc7d08d00fb80a01d6a
Mohit <mrai645@gmail.com>
2023-10-01 23:10:14 +0530

    Update ArrayList.java

--- backtracking/ArrayList.java
+++ backtracking/ArrayList.java
@@ -1,5 +1,17 @@
 package backtracking;
-
+import java.util.ArrayList;
 public class ArrayList<> {
-
+    
+    
+        public static void main(String[] args) {
+            ArrayList<Integer> list = new ArrayList<Integer>();
+    
+            //add elements
+            list.add(0);
+            list.add(2);
+            list.add(3);
+    
+            System.out.println(list);
+        }
+    }
 }
