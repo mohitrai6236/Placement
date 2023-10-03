7da06e5b0425b37a33ce6f12da3a9d6366bef381
Mohit <mrai645@gmail.com>
2023-10-03 21:42:59 +0530

    Create LL.java

---
+++ linkedlist/LL.java
@@ -0,0 +1,14 @@
+package linkedlist;
+import java.util.*;
+public class LL {
+    public static void main(String[] args) {
+
+        // Format to create a empty list
+        LinkedList<String> list = new LinkedList<String>();
+
+        list.addFirst("a");
+        list.addFirst("is");
+        System.out.println(list);
+    }
+    
+}
