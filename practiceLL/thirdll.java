6913e6aaabf443c0aa839e04b216899d1893c8c9
Mohit <mrai645@gmail.com>
2023-10-11 00:24:34 +0530

    Create thirdll.java

---
+++ practiceLL/thirdll.java
@@ -0,0 +1,23 @@
+package practiceLL;
+
+public class thirdll {
+    public boolean hasCycle(ListNode head){
+        if(head == null){
+            return false;
+        }
+        ListNode hare = head;
+        ListNode turtle = head;
+
+        while(hare!=null && hare.next!=null){
+            hare = hare.next.next;
+            turtle = turtle.next;
+
+            if(hare == turtle) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
