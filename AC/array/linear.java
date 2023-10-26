d8930c155a836220519027a249cc7ba53729c6e5
Mohit <mrai645@gmail.com>
2023-10-26 19:10:16 +0530

    Update linear.java

--- AC/array/linear.java
+++ AC/array/linear.java
@@ -1,10 +1,38 @@
 package AC.array;
 
-public class linear {
-    public static int linearSearch(int number[], int key) {
+// public class linear {
+//     public static int linearSearch(int number[], int key) {
 
-        for(int i=0;i<number.length;i++){
-            if(number[i] == key) {
+//         for(int i=0;i<number.length;i++){
+//             if(number[i] == key) {
+//                 return i;
+//             }
+//         }
+
+//         return -1;
+//     } 
+
+//     public static void main(String[] args) {
+//         int number[] = {
+//             2,4,6,8,10,12,14,16
+//         };
+//         int key = 10;
+
+//         int index = linearSearch(number, key);
+//         if(index == -1) {
+//             System.out.println("not found");
+//         } else {
+//             System.out.println("key is at index : " + index);
+//         }
+//     }
+    
+// }
+
+public class linear {
+    public static int linearSearch(String menu[], String key) {
+
+        for(int i=0;i<menu.length;i++){
+            if(menu[i] == key) {
                 return i;
             }
         }
@@ -13,12 +41,12 @@ public class linear {
     } 
 
     public static void main(String[] args) {
-        int number[] = {
-            2,4,6,8,10,12,14,16
+        String menu[] = {
+           "dosa","chowmin","idli","vada"
         };
-        int key = 10;
+        String key = "chowmin";
 
-        int index = linearSearch(number, key);
+        int index = linearSearch(menu, key);
         if(index == -1) {
             System.out.println("not found");
         } else {
